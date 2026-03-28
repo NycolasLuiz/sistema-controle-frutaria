8ccdc7b67391ad0ef620e240ecb6965fd2fd86a1
NycolasLuiz <nycolasluizsamuel@gmail.com>
2026-03-28 11:53:51 -0300

    Add fruit selection and weight input functionality

---
+++ teste.py
@@ -0,0 +1,18 @@
+print("------------------")
+print("maça: 1")
+print("banana: 2")
+print("pera: 3")
+print("uva: 4")
+print("------------------")
+fruta1 = int(input("Escolha a primeira fruta"))
+if(fruta1 == 1):
+    pesoFruta1 = float(input("Insira o peso da maçã"))
+
+if(fruta1 == 2):
+    pesoFruta1 = float(input("Insira o peso da banana"))
+fruta2 = int(input("Escolha a segunda fruta"))
+if(fruta2 == 1):
+    pesoFruta1 = float(input("Insira o peso da maçã"))
+
+if(fruta2 == 2):
+    pesoFruta1 = float(input("Insira o peso da banana"))
